d05f77f9f46cf4d45da5c867b8f2dbb6ba257ac8
andry_halo2@hotmail.com <andry_halo2@hotmail.com>
2025-10-09 00:49:23 -0600

    Qew

--- routes/premiumPayments.js
+++ routes/premiumPayments.js
@@ -30,7 +30,7 @@ module.exports = async function premiumPayments(fastify, opts) {
               {
                 title: "Activación Ghosty Premium",
                 quantity: 1,
-                unit_price: 5,
+                unit_price: 69,
                 currency_id: "MXN",
               },
             ],
